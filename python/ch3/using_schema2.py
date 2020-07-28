a5b4d747fab76aaab8ec88cb4944818d095c725a
ChadMcIntire <chadmcintirecloud@gmail.com>
2020-07-27 21:28:31 -0600

    finished with initial data inspection

--- python/ch3/using_schema2.py
+++ python/ch3/using_schema2.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, IntegerType, StringType, BooleanType, FloatType
 from pyspark.sql.functions import col, countDistinct, to_timestamp, year
+import pyspark.sql.functions as F
 
 if __name__ == "__main__":
 
@@ -74,25 +75,50 @@ if __name__ == "__main__":
     #.withColumn() is to rename, change the value, or convert the type of an
     #existing dataframe column
 
+    #2020-07-16 00:00:00
+
+    (fire_df                                # '2020-07-16 00:00:00'
+        .select("CallDate", "WatchDate", "AvailableDtTm")
+        .show(200, False))
+
     #make a column then drop the column it was formed from.
+    #change the string format it is currently in to day time format
+    #reference https://html.developreference.com/article/15793767/PySpark+dataframe+convert+unusual+string+format+to+Timestamp
     fire_ts_df = (new_fire_df
-        .withColumn("IncidentDate", to_timestamp(col("CallDate"), "MM/dd/yyyy"))
-        .drop("CallDate")
-        .withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "MM/dd/yyyy"))
+        .withColumn("IncidentDate", to_timestamp("CallDate", "yyyy-MM-dd'T'HH:mm:ss.SSS"))
+        .withColumn("Year", year('IncidentDate'))
+        .drop("CallDate")                                    #2020-07-16T00:00:00.000
+        .withColumn("OnWatchDate", to_timestamp("WatchDate", "yyyy-MM-dd'T'HH:mm:ss.SSS"))
         .drop("WatchDate")
-        .withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"), 
-        "MM/dd/yyyy hh:mm:ss a"))
+        .withColumn("AvailableDtTS", to_timestamp("AvailableDtTm",
+        "yyyy-MM-dd'T'HH:mm:ss.SSS"))
         .drop("AvailableDtTm"))
 
-    
+
+    #check out the columns we just reformated from string format to datetime 
     (fire_ts_df
         .select("IncidentDate", "OnWatchDate", "AvailableDtTS")
-        .show(5, False))
+        .show(50, False))
 
-    #(fire_ts_df
-    #    .select(year('IncindentDate'))
-    #    .district()
-    #    .orderBy(year('IncidentDate'))
-    #    .show())
+    #order by unique year combinations
+    (fire_ts_df
+        .select(year('IncidentDate'))
+        .distinct()
+        .orderBy(year('IncidentDate'))
+        .show())
 
-    
+    #Select CallType and Year, check where calltype is not null, group by year
+    #and Calltype, make a count of it, order by CallType and Year, Show the
+    #first 100
+    (fire_ts_df
+        .select("CallType", "Year")
+        .where(col("CallType").isNotNull())
+        .groupBy("CallType", "Year")
+        .count()
+        .orderBy("CallType", "Year", ascending=False)
+        .show(n=100, truncate=False))
+
+    (fire_ts_df
+        .select(F.sum("NumAlarms"), F.avg("ResponseDelayedinMins"),
+        F.min("ResponseDelayedinMins"), F.max("ResponseDelayedinMins"))
+        .show())
